5fb0215c2de737662b7e8605dfcbcedd1ceafb71
Zerakles <jonas@grewe.eu>
2023-11-08 14:54:29 +0100

    Added JavaDOC to Passwort.java

--- src/main/java/losers/club/classes/Passwort.java
+++ src/main/java/losers/club/classes/Passwort.java
@@ -18,6 +18,10 @@ public class Passwort {
     private final String password;
     private SecretKey secretKey;
 
+    /**
+     * Erstellt ein neues Passwort und einen SecretKey
+     * @param password
+     */
     public Passwort(String password){
         long seed = 233222;
         try {
@@ -28,6 +32,17 @@ public class Passwort {
         }
     }
 
+    /**
+     * Verwendet einen Seed um das erstellte Passwort zu verschlüsseln und einen SecretKey zu erstellen.
+     * @param cryptPassword
+     * @param seed
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchPaddingException
+     * @throws InvalidKeyException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     */
     private  String cryptPassword(String cryptPassword, long seed) throws NoSuchAlgorithmException, NoSuchPaddingException,
             InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
@@ -41,6 +56,15 @@ public class Passwort {
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
+    /**
+     * Diese Methode entschlüsselt das Password und gibt es zurück.
+     * @return Password
+     * @throws InvalidKeyException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     */
     private String unCryptPassword( )throws InvalidKeyException, IllegalBlockSizeException,
             BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException {
         Cipher cipher = Cipher.getInstance("AES");
@@ -49,6 +73,10 @@ public class Passwort {
         return new String (cipher.doFinal(encryptedBytes));
     }
 
+    /**
+     * Diese Methode gibt das Passwort zurück.
+     * @return
+     */
     public String getPassword(){
         try {
             return unCryptPassword();
@@ -58,6 +86,10 @@ public class Passwort {
         }
     }
 
+    /**
+     * DIese Methode gibt das verschlüsselte Passwort zurück.
+     * @return
+     */
     public String getCryptPassword(){
         return this.password;
     }
