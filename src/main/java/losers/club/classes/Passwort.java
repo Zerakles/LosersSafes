26a649394878e8d62a2a7aa12cbbabf006594fbd
Zerakles <jonas@grewe.eu>
2023-11-08 14:39:23 +0100

    Added Passwort.java - password - secretKey - unCryptPassword - cryptPassword + getCryptPassword + getPassword

--- src/main/java/losers/club/classes/Passwort.java
+++ src/main/java/losers/club/classes/Passwort.java
@@ -1,5 +1,11 @@
 package losers.club.classes;
 
+import javax.crypto.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class Passwort {
 
     /**
@@ -10,22 +16,46 @@ public class Passwort {
      */
 
     private final String password;
+    private SecretKey secretKey;
 
     public Passwort(String password){
-        this.password = cryptPassword(password);
+        long seed = 233222;
+        try {
+            this.password = cryptPassword(password, seed);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private  String cryptPassword(String cryptPassword){
-        return null;
+    private  String cryptPassword(String cryptPassword, long seed) throws NoSuchAlgorithmException, NoSuchPaddingException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.setSeed(seed);
+        keyGenerator.init(secureRandom);
+        this.secretKey = keyGenerator.generateKey();
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(cryptPassword.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
-    private String unCryptPassword(String cryptPassword){
-
-        return null;
+    private String unCryptPassword( )throws InvalidKeyException, IllegalBlockSizeException,
+            BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] encryptedBytes = Base64.getDecoder().decode(this.password);
+        return new String (cipher.doFinal(encryptedBytes));
     }
 
     public String getPassword(){
-        return unCryptPassword(this.password);
+        try {
+            return unCryptPassword();
+        } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException |
+                 IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getCryptPassword(){
